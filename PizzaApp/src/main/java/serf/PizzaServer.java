7c0bea3029c5479eca5baa86043c73ff7c485b47
Brandon Bostic <bbostic2@mix.wvu.edu>
2019-02-07 16:28:46 -0500

    Project work

--- PizzaApp/src/main/java/serf/PizzaServer.java
+++ PizzaApp/src/main/java/serf/PizzaServer.java
@@ -12,27 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 public class PizzaServer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public PizzaServer() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestHelper.process(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- PizzaApp/src/main/java/serf/RequestHelper.java
+++ PizzaApp/src/main/java/serf/RequestHelper.java
@@ -1,5 +1,16 @@
 package serf;
 
-public class RequestHelper {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import service.UserService;;
 
+public class RequestHelper {
+	public static void process(HttpServletRequest request, HttpServletResponse response) {
+		String uri = request.getRequestURI();
+		switch (uri) {
+		case "orderPizza.do":{UserService.doThing();}
+		break;
+		
+		}
+	}
 }
